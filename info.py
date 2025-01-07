1d5f1f3c9baddcb2d732111d3c1c452bfe057963
sbdipun <87560288+sbdipun@users.noreply.github.com>
2025-01-07 19:23:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,12 @@
 from os import environ
+import os
 
-API_ID = environ.get("API_ID", "")
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER_ID = int(environ.get("OWNER_ID", "1164918935"))
+API_ID = int(os.getenv("api_id", ))
+API_HASH = os.getenv("api_hash", "")
+BOT_TOKEN = os.getenv("bot_token", "")
+OWNER_ID = int(environ.get("OWNER_ID", ""))
+LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001784714143")  # Telegram Channel ID where the bot is added and have write permission. You can use group ID too.
+check_interval = int(os.environ.get("INTERVAL", 20))   # Check Interval in seconds.
+max_instances = int(environ.get("MAX_INSTANCES", 3))
+feed_urls = os.getenv("feed_urls", "https://www.thehindu.com/news/national/?service=rss|https://www.dnaindia.com/feeds/india.xml")
 
